fcdd8953b7d5dc24767debee7a9d0631ca65b9e8
Ihor Kovalenko <kovalyan27@gmail.com>
2021-06-10 23:47:01 +0300

    Update gallery-items.js

--- gallery-items.js
+++ gallery-items.js
@@ -65,28 +65,88 @@ const gallery = [
   },
 ];
 
-const galleryContainer = document.querySelector('.js-gallery');
+const galleryContainer = document.querySelector('.gallery');
+
+const ligtboxContainer = document.querySelector('.lightbox');
+
+const lightBoxCloseBut = document.querySelector('.lightbox__button');
+
+const lightBoxImg = document.querySelector('.lightbox__image');
+
+const closeBtn = document.querySelector('button[data-action="close-lightbox"]');
+
+const overlay = document.querySelector('.lightbox__overlay');
 
 const galleryMarkUp = createGalleryCards(gallery);
 
 galleryContainer.innerHTML = galleryMarkUp;
 
+galleryContainer.addEventListener('click', openModal);
+
 function createGalleryCards(gallery) {
   return gallery
     .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
-  <a
+    <a
     class="gallery__link"
     href="${preview}"
-  >
+    >
     <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
     />
-  </a>
-</li> `;
+    </a>
+    </li> `;
     })
     .join('');
+}
+
+function openModal(event) {
+  event.preventDefault();
+  if (event.target) {
+    addClass('is-open');
+    lightBoxImgContent(event.target.dataset.source, event.target.alt);
+
+    document.body.addEventListener('keydown', el => {
+      if (el.code === 'Escape') {
+        closeModal(el);
+      }
+    });
+
+    overlay.addEventListener('click', closeModal);
+
+    closeBtn.addEventListener('click', closeModal);
+  }
+}
+
+function closeModal(event) {
+  if (event.target) {
+    removeClass('is-open');
+    lightBoxImgContent('', '');
+
+    document.body.removeEventListener('keydown', el => {
+      if (el.code === 'Escape') {
+        closeModal(el);
+      }
+    });
+
+    overlay.removeEventListener('click', closeModal);
+
+    closeBtn.removeEventListener('click', closeModal);
+  }
+}
+
+function lightBoxImgContent(src, alt) {
+  lightBoxImg.src = src;
+  lightBoxImg.alt = alt;
+}
+
+function addClass(element) {
+  ligtboxContainer.classList.add(element);
+}
+
+function removeClass(element) {
+  ligtboxContainer.classList.remove(element);
 }
